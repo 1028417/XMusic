c61e66cc7dcdb8e748b4053094d685d1df8ccfeb
lhyuan <1028417@qq.com>
2019-08-12 20:54:46 +0800

    3.2.15

---
+++ inc/OptionMgr.h
@@ -0,0 +1,111 @@
+
+#pragma once
+
+struct tagPlaySpiritOption
+{
+	wstring strSkinName;
+
+	int iPosX = 500;
+	int iPosY = 500;
+
+	int iVolume = 50;
+};
+
+struct tagAlarmOption
+{
+	tagAlarmOption()
+	{
+		tmutil::getCurrentTime(nHour, nMinute);
+	}
+
+	int nHour = 0;
+	int nMinute = 0;
+
+	vector<wstring> vctAlarmmedia;
+};
+
+enum E_TimerOperate
+{
+	TO_Null = 0
+	, TO_StopPlay
+	, TO_Shutdown
+	, TO_Hibernal
+};
+
+struct tagTimerOperateOpt
+{
+	tagTimerOperateOpt()
+	{
+		eTimerOperate = TO_Null;
+
+		tmutil::getCurrentTime(nHour, nMinute);
+	}
+
+	E_TimerOperate eTimerOperate;
+
+	int nHour = 0;
+	int nMinute = 0;
+};
+
+struct tagOption
+{
+	wstring strRootDir;
+	vector<wstring> vecAttachDir;
+
+	UINT uPlayingItem = 0;
+
+    bool bRandomPlay = false;
+	bool bForce48000 = false;
+
+    bool bHideMenuBar = false;
+    bool bFullScreen = false;
+
+    wstring strHBkg;
+    wstring strVBkg;
+
+	tagPlaySpiritOption PlaySpiritOption;
+
+	tagAlarmOption AlarmOption;
+	
+	tagTimerOperateOpt TimerOption;
+};
+
+class __ModelExt COptionMgr
+{
+public:
+	COptionMgr() {}
+
+private:
+	tagOption m_Option;
+
+	bool m_bInited = false;
+	
+public:
+	tagOption& getOption()
+	{
+		return m_Option;
+	}
+
+	tagPlaySpiritOption& getPlaySpiritOption()
+	{
+		return m_Option.PlaySpiritOption;
+	}
+
+	tagAlarmOption& getAlarmOption()
+	{
+		return m_Option.AlarmOption;
+	}
+
+	tagTimerOperateOpt& getTimerOption()
+	{
+		return m_Option.TimerOption;
+	}
+
+	bool init();
+
+	wstring checkAlarm();
+
+	E_TimerOperate checkTimerOperate();
+
+	void saveOption();
+};
