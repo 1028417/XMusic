3e2f6e4112e029b838cb890e781c8623ac6454a3
lhyuan <1028417@qq.com>
2019-10-11 06:36:46 +0800

    3.2.30

--- inc/util/fsutil.h
+++ inc/util/fsutil.h
@@ -3,6 +3,10 @@
 
 #define MAX_PATH 260
 
+#if __windows
+#include <direct.h>
+#endif
+
 #if !__winvc
 #include <QFileInfo>
 #include <QFile>
@@ -198,12 +202,12 @@ public:
 #if !__winvc
     static long qCompressFile(const wstring& strSrcFile, const wstring& strDstFile, int nCompressLecvel=-1);
     static long qUncompressFile(const wstring& strSrcFile, const wstring& strDstFile);
+#endif
 
     static long zCompressFile(const wstring& strSrcFile, const wstring& strDstFile, int level=0);
     static long zUncompressFile(const wstring& strSrcFile, const wstring& strDstFile);
 
     static bool zUncompressZip(const string& strZipFile, const string& strDstDir);
-#endif
 };
 
 
