455b3839d8c42ffb5f0e8598c35b5748d0949624
musicro <1028418@qq.com>
2019-12-14 16:15:43 +0800

    3.2.76

--- src/view/src/globalSize.cpp
+++ src/view/src/globalSize.cpp
@@ -41,7 +41,7 @@ CGlobalSize::CGlobalSize()
 	m_ColWidth_MediaTag_Album = 120;
 
 	m_ColWidth_Type = 30;
-	m_ColWidth_FileSize = 58;
+	m_ColWidth_FileSize = 60;
 	m_ColWidth_RelatedPlaylist = 120;
 	m_ColWidth_RelatedSingerAlbum = 175;
 	m_ColWidth_AddTime = 82;
@@ -98,7 +98,7 @@ void CGlobalSize::init()
 	float fDPIRate = getDPIRate();
 	m_fSmallFontSize = (fHRate - 1)*1.13f * fDPIRate * fDPIRate;
 
-	m_fBigFontSize = m_fSmallFontSize * 1.76f;
+	m_fBigFontSize = m_fSmallFontSize * 1.77f;
 
 	m_fMenuFontSize = (m_fBigFontSize + m_fSmallFontSize)/2.5f;
 	m_fMenuBarFontSize = (m_fMenuFontSize + m_fSmallFontSize)/2.5f;
