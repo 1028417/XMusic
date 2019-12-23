663cd2fc01c631171a7f8e3a7d08bcc31491d33d
lhyuan <1028417@qq.com>
2019-12-24 07:32:15 +0800

    3.2.82

--- src/XMusic/cpp/medialibdlg.cpp
+++ src/XMusic/cpp/medialibdlg.cpp
@@ -97,7 +97,7 @@ void CMedialibDlg::_relayout(int cx, int cy)
 
     if (_checkBangs(cx, cy)) // 针对全面屏刘海作偏移
     {
-#define __yOffset __size(66)
+#define __yOffset __size(50)
         rcReturn.setTop(rcReturn.top()+__yOffset);
         rcReturn.setBottom(rcReturn.bottom() + __yOffset);
     }
