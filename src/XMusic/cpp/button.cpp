9b6d5bdca4ad0fe084f8e7b8210e31d704d873f5
lhyuan <1028417@qq.com>
2019-12-24 01:05:21 +0800

    3.2.81

---
+++ src/XMusic/cpp/button.cpp
@@ -0,0 +1,36 @@
+
+#include "button.h"
+
+bool CButton::event(QEvent *ev)
+{
+    if (ev->type() == QEvent::Paint)
+    {
+        setDropShadowEx();
+	}
+
+	return CWidget::event(ev);
+}
+
+void CButton::_onMouseEvent(E_MouseEventType type, const QMouseEvent&)
+{
+	if (E_MouseEventType::MET_Press == type)
+	{
+		setOpacityEffect(0.5);
+	}
+	else if (E_MouseEventType::MET_Release == type)
+	{
+		setOpacityEffect(0.5);
+
+		UINT uDelayTime = 100;
+#if __windows || __mac
+		uDelayTime = 200;
+#endif
+		timerutil::async(uDelayTime, [&](){
+			unsetOpacityEffect();
+		});
+	}
+	else if (E_MouseEventType::MET_Click == type)
+	{
+		emit signal_clicked(this);
+	}
+}
