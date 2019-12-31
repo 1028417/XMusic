87d1b9d328797a201d092b3931811165000d9c46
lhyuan <1028417@qq.com>
2019-12-31 18:29:59 +0800

    3.2.85

---
+++ src/XMusicStartup/XMusicStartup.cpp
@@ -0,0 +1,18 @@
+// XMusicStartup.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "XMusicStartup.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+	WinExec("./bin/XMusic.exe", SW_SHOW);
+
+    return 0;
+}
