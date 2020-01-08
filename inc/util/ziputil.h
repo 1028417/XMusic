07f901275f836dfff97eb2b32efb3725f282a50f
lhyuan <1028417@qq.com>
2020-01-09 02:50:19 +0800

    3.2.89

--- inc/util/ziputil.h
+++ inc/util/ziputil.h
@@ -1,10 +1,9 @@
 
 #pragma once
 
-struct __UtilExt tagUnzFileInfo
+struct __UtilExt tagUnzfile
 {
-	string strPath;
-	bool bDir = false;
+    string strPath;
 
 	size_t uFileSize = 0;
 
@@ -37,11 +36,11 @@ private:
 
 	void* m_unzfile = NULL;
 
-    list<tagUnzFileInfo> m_lstUnzDirInfo;
+    list<tagUnzfile> m_lstUnzdirInfo;
 
-    map<string, tagUnzFileInfo> m_mapUnzFileInfo;
+    map<string, tagUnzfile> m_mapUnzfileInfo;
 
-    list<tagUnzFileInfo*> m_lstUnzFileInfo;
+    list<pair<bool, tagUnzfile*>> m_lstUnzfileInfo;
 
 private:
 	bool _open(const char *szFile, void* pzlib_filefunc_def = NULL);
@@ -54,12 +53,22 @@ public:
 		return m_unzfile != NULL;
 	}
 
-    const map<string, tagUnzFileInfo>& unzfileInfoMap() const
+    const map<string, tagUnzfile>& unzfileMap() const
     {
-        return m_mapUnzFileInfo;
+        return m_mapUnzfileInfo;
     }
 
-	bool open(const string& strFile, const string& strPwd = "")
+    const list<tagUnzfile>& unzdirList() const
+    {
+        return m_lstUnzdirInfo;
+    }
+
+    const list<pair<bool, tagUnzfile*>>& unzfileList() const
+    {
+        return m_lstUnzfileInfo;
+    }
+
+    bool open(const string& strFile, const string& strPwd = "")
 	{
 		m_strPwd = strPwd;
 
@@ -70,99 +79,52 @@ public:
 
     bool unzip(const wstring& strDstDir) const;
 
-    long read(const tagUnzFileInfo& unzFileInfo, void *buf, size_t len) const;
-    long read(const string& strPath, void *buf, size_t len) const
+    long read(const tagUnzfile& unzFileInfo, CByteBuffer& bbfBuff) const
     {
-        auto itr = m_mapUnzFileInfo.find(strPath);
-        if (itr == m_mapUnzFileInfo.end())
+        if (0 == unzFileInfo.uFileSize)
+        {
+            return 0;
+        }
+
+        auto ptr = bbfBuff.resizeMore(unzFileInfo.uFileSize);
+        return _read(unzFileInfo, ptr, unzFileInfo.uFileSize);
+	}
+    long read(const string& strPath, CByteBuffer& bbfBuff) const
+    {
+        auto itr = m_mapUnzfileInfo.find(strPath);
+        if (itr == m_mapUnzfileInfo.end())
         {
             return -1;
         }
 
-        return read(itr->second, buf, len);
+        return read(itr->second, bbfBuff);
     }
 
-	template <typename T>
-    long read(const tagUnzFileInfo& unzFileInfo, TBuffer<T>& buff) const
-	{
-		long size = read(unzFileInfo, buff.ptr(), buff.size());
-		if (size > 0)
-		{
-			size /= sizeof(T);
-		}
-
-		return size;
-	}
-    template <typename T>
-    long read(const string& strPath, TBuffer<T>& buff) const
+    long read(const tagUnzfile& unzFileInfo, CCharBuffer& cbfBuff) const
     {
-        auto itr = m_mapUnzFileInfo.find(strPath);
-        if (itr == m_mapUnzFileInfo.end())
+        if (0 == unzFileInfo.uFileSize)
+        {
+            return 0;
+        }
+
+        auto ptr = cbfBuff.resizeMore(unzFileInfo.uFileSize);
+        return _read(unzFileInfo, ptr, unzFileInfo.uFileSize);
+	}
+    long read(const string& strPath, CCharBuffer& cbfBuff) const
+    {
+        auto itr = m_mapUnzfileInfo.find(strPath);
+        if (itr == m_mapUnzfileInfo.end())
         {
             return -1;
         }
 
-        return read(itr->second, buff);
-    }
-	
-    long read(const tagUnzFileInfo& unzFileInfo, CByteBuffer& bbfBuff, size_t uReadSize = 0) const
-	{
-		return _read(unzFileInfo, bbfBuff, uReadSize);
-	}
-    long read(const string& strPath, CByteBuffer& bbfBuff, size_t uReadSize = 0) const
-    {
-        auto itr = m_mapUnzFileInfo.find(strPath);
-        if (itr == m_mapUnzFileInfo.end())
-        {
-            return -1;
-        }
-
-        return read(itr->second, bbfBuff, uReadSize);
-    }
-
-    long read(const tagUnzFileInfo& unzFileInfo, CCharBuffer& cbfBuff, size_t uReadSize = 0) const
-	{
-		return _read(unzFileInfo, cbfBuff, uReadSize);
-	}
-    long read(const string& strPath, CCharBuffer& cbfBuff, size_t uReadSize = 0) const
-    {
-        auto itr = m_mapUnzFileInfo.find(strPath);
-        if (itr == m_mapUnzFileInfo.end())
-        {
-            return -1;
-        }
-
-        return read(itr->second, cbfBuff, uReadSize);
+        return read(itr->second, cbfBuff);
     }
 
 	void close();
 	
 private:
-	template <class T>
-    long _read(const tagUnzFileInfo& unzFileInfo, T& buff, size_t uReadSize) const
-	{
-		if (0 == uReadSize)
-		{
-			uReadSize = unzFileInfo.uFileSize;
-		}
-		else
-		{
-			uReadSize = MIN(uReadSize, unzFileInfo.uFileSize);
-		}
-		if (0 == uReadSize)
-		{
-			return 0;
-		}
-
-		auto ptr = buff.resizeMore(uReadSize);
-		long size = read(unzFileInfo, ptr, uReadSize);
-		if (size >= 0 && size < (long)uReadSize)
-		{
-			buff.resizeLess(uReadSize - size);
-		}
-		
-		return size;
-	}
+    long _read(const tagUnzfile& unzFileInfo, void *buf, size_t len) const;
 };
 
 class __UtilExt ziputil
