7a1252d80f072113de9b70a40b5864948f993214
musicro <1028418@qq.com>
2020-02-21 19:43:03 +0800

    3.2.95

---
+++ inc/Common/TDialog.h
@@ -0,0 +1,50 @@
+#pragma once
+
+template <UINT _ResID=0, bool _CanOK = false, bool _CanCancel = true>
+class TDialog : public CDialog
+{
+public:
+	TDialog() : CDialog(_ResID)
+	{
+	}
+
+private:
+	void OnOK() override
+	{
+		if (_CanOK)
+		{
+			OnClose(false);
+		}
+	}
+
+	void OnCancel() override
+	{
+		if (_CanCancel)
+		{
+			OnClose(true);
+		}
+	}
+
+private:
+	CCompatableFont m_font;
+
+protected:
+	virtual void PreSubclassWindow() override
+	{
+		__super::PreSubclassWindow();
+
+		m_font.setFont(*this);
+	}
+
+	virtual void OnClose(bool bCancel)
+	{
+		if (bCancel)
+		{
+			__super::OnCancel();
+		}
+		else
+		{
+			__super::OnOK();
+		}
+	}
+};
