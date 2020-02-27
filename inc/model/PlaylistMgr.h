937946082547fcc54295e5c6b8084e8c2c14ca06
musicro <1028418@qq.com>
2020-02-28 06:47:13 +0800

    3.2.96

--- inc/model/PlaylistMgr.h
+++ inc/model/PlaylistMgr.h
@@ -39,7 +39,7 @@ public:
 
 	BOOL Init();
 
-	void GetSubSets(TD_MediaSetList& lstSubSets) const override;
+    void GetSubSets(TD_MediaSetList& lstSubSets) override;
 
 	CPlaylist *AddPlaylist(UINT uPos);
 
