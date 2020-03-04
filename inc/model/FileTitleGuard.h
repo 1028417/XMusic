a3f6ef6f00fbd672872b18f4c447c974a3c0bc89
musicro <1028418@qq.com>
2020-03-05 07:09:12 +0800

    3.2.99

---
+++ inc/model/FileTitleGuard.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class __ModelExt CFileTitleGuard
+{
+public:
+	CFileTitleGuard(class CSingerMgr& SingerMgr);
+	
+private:
+	class CSingerMgr& m_SingerMgr;
+
+	PairList<wstring, wstring> m_plSingerInfo;
+
+private:
+	wstring _matchSinger(const wstring& strFileTitle);
+
+public:
+	static wstring transSingerName(const wstring& strSingerName);
+
+	wstring matchSinger(const wstring& strFileTitle);
+	wstring matchSinger(IMedia& media);
+
+	static void genCollateTitle(wstring& strFileTitle);
+	static wstring genCollateTitle_r(const wstring& strFileTitle);
+
+	static void genDisplayTitle(wstring& strFileTitle);
+	static wstring genDisplayTitle_r(const wstring& strFileTitle);
+};
