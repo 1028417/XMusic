514d28759cac01172a33f83244dec25d358ec8d7
lhyuan <1028417@qq.com>
2020-06-06 21:29:19 +0800

    3.2.117

--- deploy/winDeploy.bat
+++ deploy/winDeploy.bat
@@ -14,31 +14,31 @@ mkdir %~dp0XMusic-win32\bin\platforms
 
 cd /d ../bin
 
-copy /Y avutil-56.dll ..\release\XMusic-win32\bin
-copy /Y avformat-58.dll ..\release\XMusic-win32\bin
-copy /Y avcodec-58.dll ..\release\XMusic-win32\bin
-copy /Y swresample-3.dll ..\release\XMusic-win32\bin
+copy /Y avutil-56.dll ..\deploy\XMusic-win32\bin
+copy /Y avformat-58.dll ..\deploy\XMusic-win32\bin
+copy /Y avcodec-58.dll ..\deploy\XMusic-win32\bin
+copy /Y swresample-3.dll ..\deploy\XMusic-win32\bin
 
-copy /Y SDL2.dll ..\release\XMusic-win32\bin
+copy /Y SDL2.dll ..\deploy\XMusic-win32\bin
 
-copy /Y XMusic.exe ..\release\XMusic-win32\bin
+copy /Y XMusic.exe ..\deploy\XMusic-win32\bin
 
-copy /Y x*.dll ..\release\XMusic-win32\bin
+copy /Y x*.dll ..\deploy\XMusic-win32\bin
 
-copy /Y lib*.dll ..\release\XMusic-win32\bin
+copy /Y lib*.dll ..\deploy\XMusic-win32\bin
 
-copy /Y Qt5*.dll ..\release\XMusic-win32\bin
+copy /Y Qt5*.dll ..\deploy\XMusic-win32\bin
 
-copy font\msyhl-6.23.ttc ..\release\XMusic-win32\bin\font
+copy font\msyhl-6.23.ttc ..\deploy\XMusic-win32\bin\font
 
-copy  %~dp0..\..\bkg\* ..\release\XMusic-win32\bin\bkg
-copy  %~dp0..\..\bkg\hbkg\* ..\release\XMusic-win32\bin\bkg\hbkg
-copy  %~dp0..\..\bkg\vbkg\* ..\release\XMusic-win32\bin\bkg\vbkg
-copy  %~dp0..\..\bkg\hbkg\city\* ..\release\XMusic-win32\bin\bkg\hbkg\city
-copy  %~dp0..\..\bkg\vbkg\city\* ..\release\XMusic-win32\bin\bkg\vbkg\city
+copy  %~dp0..\..\bkg\* ..\deploy\XMusic-win32\bin\bkg
+copy  %~dp0..\..\bkg\hbkg\* ..\deploy\XMusic-win32\bin\bkg\hbkg
+copy  %~dp0..\..\bkg\vbkg\* ..\deploy\XMusic-win32\bin\bkg\vbkg
+copy  %~dp0..\..\bkg\hbkg\city\* ..\deploy\XMusic-win32\bin\bkg\hbkg\city
+copy  %~dp0..\..\bkg\vbkg\city\* ..\deploy\XMusic-win32\bin\bkg\vbkg\city
 
-copy iconengines\* ..\release\XMusic-win32\bin\iconengines
-copy imageformats\* ..\release\XMusic-win32\bin\imageformats
-copy platforms\* ..\release\XMusic-win32\bin\platforms
+copy iconengines\* ..\deploy\XMusic-win32\bin\iconengines
+copy imageformats\* ..\deploy\XMusic-win32\bin\imageformats
+copy platforms\* ..\deploy\XMusic-win32\bin\platforms
 
 pause
