781e8f5435cc9cc41733d8bd1d1baffe5ada1ff1
lhyuan <1028417@qq.com>
2020-06-07 20:37:19 +0800

    3.2.118

---
+++ inc/util/jniutil.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+class __UtilExt jniutil
+{
+public:
+	static int g_jniVer;
+
+    //安卓6以上需要动态申请权限
+#if (QT_VERSION >= QT_VERSION_CHECK(5,10,0)) // Qt5.10以上
+    static bool requestAndroidPermission(cqstr qsPermission);
+#endif
+
+	static bool checkMobileConnected();
+
+	static void vibrate();
+
+    static void installApk(cqstr qsApkPath);
+};
