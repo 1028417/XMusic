cd4dce6c7ebc9f2947c601e7d19e56bbc23efa94
lhyuan <1028417@qq.com>
2020-06-11 10:46:19 +0800

    3.2.119

---
+++ src/XMusic/cpp/androidutil.cpp
@@ -0,0 +1,108 @@
+
+#include "util/util.h"
+
+#include "androidutil.h"
+
+//#include <jni.h>
+//#include <QAndroidJniEnvironment>
+//#include <QAndroidJniObject>
+#include <QtAndroid>
+//#include <QtAndroidExtras>
+
+int g_jniVer = 0;
+int g_androidSdkVer = 0;
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved)
+{
+    (void)reserved;
+
+    g_androidSdkVer = QtAndroid::androidSdkVersion();
+
+    JNIEnv* env = nullptr;
+    if (vm->GetEnv((void**) &env, JNI_VERSION_1_6) == JNI_OK)
+    {
+        return g_jniVer = JNI_VERSION_1_6;
+    }
+    else if (vm->GetEnv((void**) &env, JNI_VERSION_1_4) == JNI_OK)
+    {
+        return g_jniVer = JNI_VERSION_1_4;
+    }
+    else if (vm->GetEnv((void**) &env, JNI_VERSION_1_2) == JNI_OK)
+    {
+        return g_jniVer = JNI_VERSION_1_2;
+    }
+    else if (vm->GetEnv((void**) &env, JNI_VERSION_1_1) == JNI_OK)
+    {
+        return g_jniVer = JNI_VERSION_1_1;
+    }
+
+    return JNI_ERR;
+}
+
+//？？QAndroidJniObject::getStaticField<jint>("android/os/Build/VERSION", "SDK_INT");
+
+/*bool androidutil::checkBuildSdkVerion(const char *pszVersion)
+{
+    return buildSdkVerion() == QAndroidJniObject::getStaticField(
+            "android/os/Build/VERSION_CODES", pszVersion);
+}*/
+
+//API 23以上需要动态申请权限
+#if (QT_VERSION >= QT_VERSION_CHECK(5,10,0)) // Qt5.10以上
+bool androidutil::requestAndroidPermission(cqstr qsPermission)
+{
+    auto ret = QtAndroid::checkPermission(qsPermission);
+    if(QtAndroid::PermissionResult::Granted == ret)
+    {
+        return true;
+    }
+
+    QtAndroid::requestPermissionsSync( QStringList() << qsPermission );
+
+    return QtAndroid::PermissionResult::Granted == QtAndroid::checkPermission(qsPermission);
+}
+#endif
+
+static QAndroidJniObject _getService(const char *pszName)
+{
+    cauto jniName = QAndroidJniObject::getStaticObjectField(
+                "android/content/Context",
+                pszName,
+                "Ljava/lang/String;");
+    if (!jniName.isValid())
+    {
+        return QAndroidJniObject();
+    }
+
+    return QtAndroid::androidActivity().callObjectMethod(
+                "getSystemService",
+                "(Ljava/lang/String;)Ljava/lang/Object;",
+                jniName.object<jstring>());
+}
+
+// Z -- jboolean -- bllean
+// I -- jint -- int;
+// J -- jlong -- long
+bool androidutil::checkMobileConnected()
+{
+    return QtAndroid::androidActivity().callMethod<jboolean>("checkMobileConnected");
+}
+
+void androidutil::vibrate()
+{
+    cauto jniService = _getService("VIBRATOR_SERVICE");
+    if (!jniService.isValid())
+    {
+        return;
+    }
+    jniService.callMethod<void>("vibrate", "(J)V", jlong(100));
+}
+
+void androidutil::installApk(cqstr qsApkPath)
+{
+    // TODO 动态申请安装未知来源应用的权限(据说有些手机不提示未知来源应用)
+    cauto jsApkPath = QAndroidJniObject::fromString(qsApkPath);
+    QtAndroid::androidActivity().callMethod<void>("installApk",
+                "(Ljava/lang/String;)V",
+                jsApkPath.object<jstring>());
+}
---
+++ src/XMusic/cpp/androidutil.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+class androidutil
+{
+public:
+    //API 23以上需要动态申请权限
+#if (QT_VERSION >= QT_VERSION_CHECK(5,10,0)) // Qt5.10以上
+    static bool requestAndroidPermission(cqstr qsPermission);
+#endif
+
+	static bool checkMobileConnected();
+
+	static void vibrate();
+
+    static void installApk(cqstr qsApkPath);
+};
